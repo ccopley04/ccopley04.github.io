9c96143a5f5c3ba0dcbf5824e324269699f09dce
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-07-23 17:22:08 -0400

    Added link previews
    When the user hovers over the redirection links, a preview image of where ever the link leads appears. It has a blue border and appears directly above the three links.

--- interactButtons.js
+++ interactButtons.js
@@ -1,11 +1,21 @@
 const buttons = document.querySelectorAll(".redirectButton");
+const pic = document.getElementById("linkPic");
+
+const picLinks = [
+  "/Images/resumePlaceholder.jpg",
+  "/Images/linkedInPlaceholder.jpg",
+  "/Images/gitHubPlaceholder.jpg",
+];
 
 buttons.forEach((myButton, index) => {
   myButton.addEventListener("mouseenter", () => {
     myButton.style = "background-color: blue";
+    pic.src = picLinks[index];
+    pic.style = "opacity:1";
   });
 
   myButton.addEventListener("mouseleave", () => {
     myButton.style = "background-color: white";
+    pic.style = "opacity:0";
   });
 });
