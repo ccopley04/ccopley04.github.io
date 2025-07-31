e40e3059ebe28e4df892fef15680c00fb8bf3bbb
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-07-31 16:43:54 -0400

    Added comments and new GitHub pic
    Added a new picture for my GitHub link. Also added comments to all scripts to explain code better for viewers.

--- Scripts/darkMode.js
+++ Scripts/darkMode.js
@@ -1,17 +1,22 @@
+//Retrieve the dark mode toggle
 const toggle = document.getElementById("darkMode");
 
-let body = document.body;
-let underline = document.getElementById("intro-separator");
-let redirectButtons = document.getElementsByClassName("redirectButton");
-let projects = document.getElementsByClassName("project");
-let linkPic = document.getElementById("linkPic");
-let projectHeader = document.getElementById("projectHeader");
-let centerLine = document.getElementById("centerLine");
-let titles = document.getElementsByClassName("title");
-let underlines = document.getElementsByClassName("underline");
-let connectPrompt = document.getElementById("connectPrompt");
+//Retrieve all HTML elements that change color with dark mode
+const body = document.body;
+const underline = document.getElementById("intro-separator");
+const redirectButtons = document.getElementsByClassName("redirectButton");
+const projects = document.getElementsByClassName("project");
+const linkPic = document.getElementById("linkPic");
+const projectHeader = document.getElementById("projectHeader");
+const centerLine = document.getElementById("centerLine");
+const titles = document.getElementsByClassName("title");
+const underlines = document.getElementsByClassName("underline");
+const connectPrompt = document.getElementById("connectPrompt");
+const bottom = document.getElementById("bottom");
 
+//Whenever the toggle is clicked
 toggle.addEventListener("change", () => {
+  //Toggle dark mode on all retrieved HTML elements
   centerLine.classList.toggle("dark-mode");
   body.classList.toggle("dark-mode");
   underline.classList.toggle("dark-mode");
@@ -31,4 +36,5 @@ toggle.addEventListener("change", () => {
     underline.classList.toggle("dark-mode");
   });
   connectPrompt.classList.toggle("dark-mode");
+  bottom.classList.toggle("dark-mode");
 });
