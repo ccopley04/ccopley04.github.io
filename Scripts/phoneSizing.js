cd1217e134d1856cfbb5f6f0579a45cf9b19b5e0
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-08-10 17:12:41 -0400

    Made webpage sizing dynamic
    Added a CSS file to hold all the CSS for the images that are in each project. Added an event listener attached to window resizing. When the window changes size enough, the window reloads.

--- Scripts/phoneSizing.js
+++ Scripts/phoneSizing.js
@@ -10,7 +10,7 @@ if (phoneSize) {
   //Add the headshot
   const top = document.getElementById("top");
   top.style =
-    "display:grid; grid-template-columns:30vw 30vw 30vw; grid-template-row:1vh 9vh";
+    "display:grid; grid-template-columns:30vw 42vw 18vw; grid-template-row:1vh 9vh";
 
   top.appendChild(headShot);
   headShot.style = "grid-column:3; grid-row: span 2 / span 2;";
@@ -47,9 +47,25 @@ if (phoneSize) {
   //If window is phone, change the project header height
   const projectHeader = document.getElementById("projectHeader");
   projectHeader.classList.toggle("phone");
+
+  //Make sure all images take up no space and have no source
+  const images = document.getElementsByClassName("image");
+  Array.from(images).forEach((image) => {
+    image.style = "";
+    image.classList.toggle("phone");
+  });
 } else {
   //If the webpage is not opened with a phone, place image in intro div
   const intro = document.getElementById("intro");
   intro.appendChild(headShot);
   intro.appendChild(document.createElement("br"));
 }
+
+//If the window is resized to phone or not phone, reload the page
+window.addEventListener("resize", () => {
+  let newPhoneSize = window.innerWidth < window.innerHeight;
+  if (phoneSize != newPhoneSize) {
+    window.location.reload();
+    phoneSize = newPhoneSize;
+  }
+});
